67f9da7dfc5f2ff66538d446b96d1b7eeb649ea1
Adeyemi-Timilehin <adeyemitimmy@gmail.com>
2022-10-05 13:21:55 +0100

    added new vesion to travis and updated the environment and production

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a768242b581924408aea45fdd9059ab5-2143635595.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
